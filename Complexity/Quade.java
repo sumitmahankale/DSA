6e2d88999621fe4ab994c648edb13edbd2116a9f
sumitmahankale <sumitmahankale7777@gmail.com>
2025-04-06 07:35:16 +0530

    Updated

---
+++ Complexity/Quade.java
@@ -0,0 +1,22 @@
+package Complexity;
+
+public class Quade {
+    public static void main(String[] args) {
+        int arr[]=new int[]{1,2,3,4,5,6,7,8,9};
+    int target=9;
+
+    //Find pairs whose sum is the target (9)
+
+    for(int i=0;i<=arr.length;i++)
+    {
+        for(int j=i+1;j<arr.length;j++)
+        {
+            if(arr[i]+arr[j]==target)
+            {
+                System.out.println("Pair:"+arr[i]+ " "+ arr[j]);
+            }
+        }
+    }
+    }
+    
+}
